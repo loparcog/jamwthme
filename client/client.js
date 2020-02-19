ce98edc654e71a48fdd25ea4cecf1965d1c7e8cd
Giacomo <gloparco@gmail.com>
2020-02-18 20:45:59 -0500

    Okay so I'm gonna be real with you I want a backup for the code right now so I can restructure using the UI first

--- client/client.js
+++ client/client.js
@@ -1,3 +1,10 @@
+// JAM CLIENT JS CODE
+// GIACOMO LOPARCO
+
+///
+// FRONTEND FUNCTIONS
+///
+
 // Get all html elements
 window.onload = function(){
     var frontPage = document.getElementById("frontPage");
@@ -7,6 +14,8 @@ window.onload = function(){
     var btnAbout = document.getElementById("btnabout");
 }
 
+
+
 function startApp() 
 {
     // Hide the menu
@@ -15,17 +24,50 @@ function startApp()
     // Load in the tone engine
     var synth = new Tone.Synth().toMaster()
 
-    synth.triggerAttackRelease('C4', '8n')
+    synth.triggerAttackRelease('G1', '8n')
 
     var rack = new Nexus.Rack("#appDiv");
+    var piano = new Nexus.Piano("#appDiv", {
+        'size': [1000, 300]
+    });
 
     rack.freq.value = 0.25;
 
     // Show the app div
     appPage.style.display = "inline-block";
+
+    // Declare any events
+    piano.on('change', function(x){
+        console.log(x);
+        if (x.state == true)
+        {
+            synth.triggerAttackRelease(toNote(x.note), '8n')
+        }
+    });
+
+    rack.freq.on('change', function(v){
+        console.log(v * 1000);
+        synth.frequency = v * 1000;
+        console.log(synth.frequency.value)
+    });
+
 }
 
 function showAbt()
 {
+
+}
+
+///
+// UTILITY FUNCTIONS
+///
+
+function toNote(x)
+{
+    // Middle C is 36, sharps and flats denoted by # and b, respectively
+    var notes = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#","A", "A#", "B"];
+    var rem = x % 12;
+    var quo = Math.floor(x/12) + 1;
+    return notes[rem] + quo.toString();
 
 }
